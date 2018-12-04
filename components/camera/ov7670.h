0228ebc5caba8b09e6e804a11503a4190c37b44d
Kyle Miller <kmille42@nd.edu>
2018-12-04 13:59:54 -0500

    Initial Commit

---
+++ components/camera/ov7670.h
@@ -0,0 +1,12 @@
+/* 
+* This work is licensed under the MIT license, see the file LICENSE for details.
+* Kyle Miller
+*
+*
+* OV7670 driver.
+*/
+#ifndef __OV7670_H__
+#define __OV7670_H__
+#include "sensor.h"
+int ov7670_init(sensor_t *sensor);
+#endif // __OV7670_H__
---
+++ main/app_main.c
@@ -0,0 +1,282 @@
+// Code for taking a picture with the esp 32 and sending it to aws 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "nvs_flash.h"
+
+#include "driver/gpio.h"
+#include "camera.h"
+#include "bitmap.h"
+
+#include "aws_iot_config.h"
+#include "aws_iot_log.h"
+#include "aws_iot_version.h"
+#include "aws_iot_mqtt_client_interface.h"
+
+#define CAMERA_PIXEL_FORMAT CAMERA_PF_JPEG
+#define CAMERA_FRAME_SIZE CAMERA_FS_SVGA
+static camera_pixelformat_t s_pixel_format;
+
+#define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
+#define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
+
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+
+extern const uint8_t aws_root_ca_pem_start[] asm("_binary_aws_root_ca_pem_start");
+extern const uint8_t aws_root_ca_pem_end[] asm("_binary_aws_root_ca_pem_end");
+extern const uint8_t certificate_pem_crt_start[] asm("_binary_certificate_pem_crt_start");
+extern const uint8_t certificate_pem_crt_end[] asm("_binary_certificate_pem_crt_end");
+extern const uint8_t private_pem_key_start[] asm("_binary_private_pem_key_start");
+extern const uint8_t private_pem_key_end[] asm("_binary_private_pem_key_end");
+
+/**
+ * @brief Default MQTT HOST URL is pulled from the aws_iot_config.h
+ */
+char HostAddress[255] = AWS_IOT_MQTT_HOST;
+
+/**
+ * @brief Default MQTT port is pulled from the aws_iot_config.h
+ */
+uint32_t port = AWS_IOT_MQTT_PORT;
+
+static const char* TAG = "awscam";
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch (event->event_id) {
+        case SYSTEM_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            esp_wifi_connect();
+            xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_WIFI_SSID,
+            .password = EXAMPLE_WIFI_PASS,
+        },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+void aws_iot_task(void *param) {
+    uint8_t * img;
+    size_t chunk_size = 480;
+
+    IoT_Error_t rc = FAILURE;
+
+    AWS_IoT_Client client;
+    IoT_Client_Init_Params mqttInitParams = iotClientInitParamsDefault;
+    IoT_Client_Connect_Params connectParams = iotClientConnectParamsDefault;
+
+    IoT_Publish_Message_Params paramsQOS0;
+
+    ESP_LOGI(TAG, "AWS IoT SDK Version %d.%d.%d-%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
+
+    mqttInitParams.enableAutoReconnect = false; // We enable this later below
+    mqttInitParams.pHostURL = HostAddress;
+    mqttInitParams.port = port;
+    mqttInitParams.pRootCALocation = (const char *)aws_root_ca_pem_start;
+    mqttInitParams.pDeviceCertLocation = (const char *)certificate_pem_crt_start;
+    mqttInitParams.pDevicePrivateKeyLocation = (const char *)private_pem_key_start;
+
+    mqttInitParams.mqttCommandTimeout_ms = 20000;
+    mqttInitParams.tlsHandshakeTimeout_ms = 5000;
+    mqttInitParams.isSSLHostnameVerify = true;
+    mqttInitParams.disconnectHandler = NULL;
+    mqttInitParams.disconnectHandlerData = NULL;
+
+    rc = aws_iot_mqtt_init(&client, &mqttInitParams);
+    if(SUCCESS != rc) {
+        ESP_LOGE(TAG, "aws_iot_mqtt_init returned error : %d ", rc);
+        abort();
+    }
+
+    /* Wait for WiFI to show as connected */
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+                        false, true, portMAX_DELAY);
+
+    connectParams.keepAliveIntervalInSec = 10;
+    connectParams.isCleanSession = true;
+    connectParams.MQTTVersion = MQTT_3_1_1;
+    /* Client ID is set in the menuconfig of the example */
+    connectParams.pClientID = CONFIG_AWS_EXAMPLE_CLIENT_ID;
+    connectParams.clientIDLen = (uint16_t) strlen(CONFIG_AWS_EXAMPLE_CLIENT_ID);
+    connectParams.isWillMsgPresent = false;
+
+    ESP_LOGI(TAG, "Connecting to AWS...");
+    do {
+        rc = aws_iot_mqtt_connect(&client, &connectParams);
+        if(SUCCESS != rc) {
+            ESP_LOGE(TAG, "Error(%d) connecting to %s:%d", rc, mqttInitParams.pHostURL, mqttInitParams.port);
+            vTaskDelay(1000 / portTICK_RATE_MS);
+        }
+    } while(SUCCESS != rc);
+
+    /*
+     * Enable Auto Reconnect functionality. Minimum and Maximum time of Exponential backoff are set in aws_iot_config.h
+     *  #AWS_IOT_MQTT_MIN_RECONNECT_WAIT_INTERVAL
+     *  #AWS_IOT_MQTT_MAX_RECONNECT_WAIT_INTERVAL
+     */
+    rc = aws_iot_mqtt_autoreconnect_set_status(&client, true);
+    if(SUCCESS != rc) {
+        ESP_LOGE(TAG, "Unable to set Auto Reconnect to true - %d", rc);
+        abort();
+    }
+
+    const char *TOPIC = "topic/camerafeed";
+    const int TOPIC_LEN = strlen(TOPIC);
+
+    /*
+    ESP_LOGI(TAG, "Subscribing...");
+    rc = aws_iot_mqtt_subscribe(&client, TOPIC, TOPIC_LEN, QOS0, iot_subscribe_callback_handler, NULL);
+    if(SUCCESS != rc) {
+        ESP_LOGE(TAG, "Error subscribing : %d ", rc);
+        abort();
+    }
+	*/
+	esp_err_t err = camera_run();
+	if (err != ESP_OK) {
+		ESP_LOGD(TAG, "Camera capture failed with error = %d", err);
+		abort();
+	}
+	img = camera_get_fb();
+
+    paramsQOS0.qos = QOS0;
+    paramsQOS0.payload = (void *) img;
+    paramsQOS0.isRetained = 0;
+
+    while((NETWORK_ATTEMPTING_RECONNECT == rc || NETWORK_RECONNECTED == rc || SUCCESS == rc)) {
+
+        //Max time the yield function will wait for read messages
+        rc = aws_iot_mqtt_yield(&client, 100);
+        if(NETWORK_ATTEMPTING_RECONNECT == rc) {
+            // If the client is attempting to reconnect we will skip the rest of the loop.
+            continue;
+        }
+
+        ESP_LOGI(TAG, "Stack remaining for task '%s' is %d bytes", pcTaskGetTaskName(NULL), uxTaskGetStackHighWaterMark(NULL));
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        size_t total_size = camera_get_data_size();
+        ESP_LOGI(TAG, "Total Image Size is: %u", total_size);
+        size_t counter = 0;
+        while (counter < total_size) {
+        	paramsQOS0.payloadLen = ((total_size - counter) < chunk_size) ? (total_size - counter) : chunk_size;
+        	paramsQOS0.payload = ((void *) img) + counter;
+        	ESP_LOGI(TAG, "Payload Length is: %u", paramsQOS0.payloadLen);
+        	rc = aws_iot_mqtt_publish(&client, TOPIC, TOPIC_LEN, &paramsQOS0);
+        	if (SUCCESS != rc) {
+        		ESP_LOGE(TAG, "Publish was unsuccessful with error = %d", rc);
+        	}
+        	counter += chunk_size;
+        }
+        vTaskDelay(15000/portTICK_RATE_MS);
+
+        err = camera_run();
+		if (err != ESP_OK) {
+			ESP_LOGD(TAG, "Camera capture failed with error = %d", err);
+			abort();
+		}
+		img = camera_get_fb();
+    }
+
+    ESP_LOGE(TAG, "An error occurred in the main loop.");
+    abort();
+}
+
+void app_main() {
+	esp_log_level_set("wifi", ESP_LOG_WARN);
+    esp_log_level_set("gpio", ESP_LOG_WARN);
+
+    esp_err_t err = nvs_flash_init();
+    if (err != ESP_OK) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        ESP_ERROR_CHECK( nvs_flash_init() );
+    }
+
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
+
+    camera_config_t camera_config = {
+        .ledc_channel = LEDC_CHANNEL_0,
+        .ledc_timer = LEDC_TIMER_0,
+        .pin_d0 = CONFIG_D0,
+        .pin_d1 = CONFIG_D1,
+        .pin_d2 = CONFIG_D2,
+        .pin_d3 = CONFIG_D3,
+        .pin_d4 = CONFIG_D4,
+        .pin_d5 = CONFIG_D5,
+        .pin_d6 = CONFIG_D6,
+        .pin_d7 = CONFIG_D7,
+        .pin_xclk = CONFIG_XCLK,
+        .pin_pclk = CONFIG_PCLK,
+        .pin_vsync = CONFIG_VSYNC,
+        .pin_href = CONFIG_HREF,
+        .pin_sscb_sda = CONFIG_SDA,
+        .pin_sscb_scl = CONFIG_SCL,
+        .pin_reset = CONFIG_RESET,
+        .xclk_freq_hz = CONFIG_XCLK_FREQ,
+    };
+
+    camera_model_t camera_model;
+    err = camera_probe(&camera_config, &camera_model);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera probe failed with error 0x%x", err);
+        return;
+    }
+
+    if (camera_model == CAMERA_OV2640) {
+    	ESP_LOGI(TAG, "Detected OV2640 camera, using JPEG format");
+        s_pixel_format = CAMERA_PF_JPEG;
+        camera_config.frame_size = CAMERA_FRAME_SIZE;
+        camera_config.jpeg_quality = 10;
+    }
+    else {
+    	ESP_LOGE(TAG, "Camera not supported");
+    	return;
+    }
+
+    camera_config.pixel_format = s_pixel_format;
+    err = camera_init(&camera_config);
+    if (err != ESP_OK) {
+    	ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
+    	return;
+    }
+
+    initialise_wifi();
+    xTaskCreatePinnedToCore(&aws_iot_task, "aws_iot_task", 9216, NULL, 5, NULL, 1);
+	
+}
